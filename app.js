3e83e9aa8ad5abdef898e4baf7b53e8c83a00d3a
Nick Fletcher <nickeax@gmail.com>
2020-07-02 09:34:12 +1000

    Beginnings of app.

---
+++ app.js
@@ -0,0 +1,18 @@
+// REQUIRES
+const express = require('express')
+const graphql = require('express-graphql')
+const schema = require('schema');
+// VARS
+const PORT = process.env.PORT || 4000
+const app = express()
+
+// MIDDLEWARES
+app.use('/graphql', graphql({
+  schema: schema,
+  graphiql: true
+}))
+
+// SERVER OPS
+app.listen(PORT, _ => {
+  console.log(`Listening on port: ${PORT}`);
+})
